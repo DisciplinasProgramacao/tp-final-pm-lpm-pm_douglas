8a776a89b3e08ff9c67d0f08fc00c2f29c0dc343
dgsilveira <dgsilveira@sga.pucminas.br>
2022-06-02 13:20:22 -0300

    Create Empolgados.java

---
+++ XulambGames/src/entities/Empolgados.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class Empolgados extends Cliente{
+    /**
+     * Construtor.
+     * Não é permitido parâmetro null ou menor que 5 dígitos.
+     *
+     * @param nome        Nome do cliente.
+     * @param nomeUsuario Nome de usuário.
+     * @param senha       Senha do cliente.
+     */
+    public Empolgados(String nome, String nomeUsuario, String senha) {
+        super(nome, nomeUsuario, senha);
+    }
+
+    @Override
+    public int calculaDesconto() {
+        return 0;
+    }
+
+    @Override
+    public boolean pagaMensalidade() {
+        return true;
+    }
+
+    @Override
+    public int calculaMensalidade() {
+        return 0;
+    }
+}
