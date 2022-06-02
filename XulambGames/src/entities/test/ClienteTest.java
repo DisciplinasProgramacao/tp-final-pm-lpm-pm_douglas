97b966d2699286df4b60d9a6c645e7cbf6b2f1f9
dgsilveira <dgsilveira@sga.pucminas.br>
2022-06-02 00:02:07 -0300

    Movido Cliente Test

---
+++ XulambGames/src/entities/test/ClienteTest.java
@@ -0,0 +1,19 @@
+package entities.test;
+
+import entities.Cliente;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ClienteTest {
+    @Test
+    void criaTest() {
+        Cliente cliente1 = new Cliente("Douglas", "doug", "1234");
+        String s = null;
+        Cliente cliente2 = new Cliente("s", "s", "s");
+
+
+
+    }
+
+}
