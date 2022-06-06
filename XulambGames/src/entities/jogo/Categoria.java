16073574493a31bc0c46e880596b48f69d8e30cd
Leticia <leticia.fraga@mindsdigital.net>
2022-06-06 19:30:17 -0300

    update categorias jogos

--- XulambGames/src/entities/jogo/Categoria.java
+++ XulambGames/src/entities/jogo/Categoria.java
@@ -1,8 +1,29 @@
 package entities.jogo;
 
 public enum Categoria {
-    LANCAMENTO,
-    PREMIUM,
-    REGULAR,
-    PROMOCAO;
+    LANCAMENTO(1.1, 1.1, "Lancamento"),
+    PREMIUM(1, 1, "Premium"),
+    REGULAR(0.7, 1, "Regular"),
+    PROMOCAO(0.3, 0.5, "Promocao");
+
+    private double miltiplicadorMax;
+    private double multiplicadorMin;
+    private String nome;
+
+    public double maiorPreco(){
+        return this.multiplicadorMin;
+    }
+
+    public double menorPreco(){
+        return this.multiplicadorMin;
+    }
+
+    public String nome(){
+        return this.nome;
+    }
+
+    @Override
+    public String toString(){
+        return this.nome();
+    }
 }
